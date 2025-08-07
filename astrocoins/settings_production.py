1d9f1c3edcafd10ce2826fd1f8ff429faa6f6003
Astrocoins Developer <akbaralievsarvar4@yandex.com>
2025-08-07 21:09:40 +1000

    Fix TEMPLATES settings conflict - inherit from main settings.py instead of overriding

--- astrocoins/settings_production.py
+++ astrocoins/settings_production.py
@@ -10,22 +10,8 @@ DEBUG = False
 # Разрешенные хосты
 ALLOWED_HOSTS = ['algoritmika25.store', 'www.algoritmika25.store', 'localhost', '127.0.0.1']
 
-# Настройки шаблонов для кастомных страниц ошибок
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'core/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+# Настройки шаблонов наследуются из settings.py
+# (core/templates уже включён в основных настройках)
 
 # Настройки безопасности для продакшена
 SECURE_SSL_REDIRECT = True
